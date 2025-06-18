9fedd8c8649cc69252e2a9797902e6cf925b90d4
Youssef-Salem6 <youssefsalem383@gmail.com>
2025-06-18 19:04:23 +0300

    loclization

--- lib/generated/intl/messages_ar.dart
+++ lib/generated/intl/messages_ar.dart
@@ -22,8 +22,243 @@ class MessageLookup extends MessageLookupByLibrary {
 
   final messages = _notInlinedMessages(_notInlinedMessages);
   static Map<String, Function> _notInlinedMessages(_) => <String, Function>{
-    "conttinue": MessageLookupByLibrary.simpleMessage("استمرار"),
-    "finish": MessageLookupByLibrary.simpleMessage("انهاء"),
+    "ForgetPassword": MessageLookupByLibrary.simpleMessage("نسيت كلمة المرور"),
+    "Password": MessageLookupByLibrary.simpleMessage("كلمة المرور"),
+    "additionalSettings": MessageLookupByLibrary.simpleMessage(
+      "إعدادات إضافية",
+    ),
+    "address": MessageLookupByLibrary.simpleMessage("العنوان"),
+    "age": MessageLookupByLibrary.simpleMessage("العمر"),
+    "activeate": MessageLookupByLibrary.simpleMessage("تفعيل"),
+    "deActivate": MessageLookupByLibrary.simpleMessage("الغاء التفعيل"),
+    "analytics": MessageLookupByLibrary.simpleMessage("التحليلات"),
+    "approved": MessageLookupByLibrary.simpleMessage("تمت الموافقة"),
+    "callPatient": MessageLookupByLibrary.simpleMessage("اتصال بالمريض"),
+    "changePassword": MessageLookupByLibrary.simpleMessage("تغيير كلمة المرور"),
+    "complaint": MessageLookupByLibrary.simpleMessage("شكوى"),
+    "complaintSubmittedSuccessfully": MessageLookupByLibrary.simpleMessage(
+      "تم إرسال شكواك بنجاح",
+    ),
+    "complaints": MessageLookupByLibrary.simpleMessage("الشكاوى"),
+    "confermationPassword": MessageLookupByLibrary.simpleMessage(
+      "تأكيد كلمة المرور",
+    ),
+    "confermationPasswordError": MessageLookupByLibrary.simpleMessage(
+      "يجب أن تتطابق كلمة المرور مع تأكيد كلمة المرور",
+    ),
+    "contactUs": MessageLookupByLibrary.simpleMessage("اتصل بنا"),
+    "conttinue": MessageLookupByLibrary.simpleMessage("متابعة"),
+    "createComplaint": MessageLookupByLibrary.simpleMessage("إنشاء شكوى"),
+    "date": MessageLookupByLibrary.simpleMessage("التاريخ"),
+    "descriptionFieldHint": MessageLookupByLibrary.simpleMessage(
+      "صف شكواك بالتفصيل",
+    ),
+    "descriptionFieldLabel": MessageLookupByLibrary.simpleMessage("الوصف"),
+    "descriptionValidationEmptyError": MessageLookupByLibrary.simpleMessage(
+      "الرجاء وصف شكواك",
+    ),
+    "descriptionValidationLengthError": MessageLookupByLibrary.simpleMessage(
+      "يجب أن يكون الوصف 10 أحرف على الأقل",
+    ),
+    "didnotReciveEmail": MessageLookupByLibrary.simpleMessage(
+      "لم تستلم البريد الإلكتروني؟ ",
+    ),
+    "documents": MessageLookupByLibrary.simpleMessage("المستندات"),
+    "documentsReviewDescription": MessageLookupByLibrary.simpleMessage(
+      "يمكنك تتبع حالة مستنداتك هنا",
+    ),
+    "dontHaveAccount": MessageLookupByLibrary.simpleMessage("ليس لديك حساب؟"),
+    "editProfile": MessageLookupByLibrary.simpleMessage("تعديل الملف الشخصي"),
+    "email": MessageLookupByLibrary.simpleMessage("البريد الإلكتروني"),
+    "emailUs": MessageLookupByLibrary.simpleMessage("راسلنا عبر البريد"),
+    "embtyAddressWarning": MessageLookupByLibrary.simpleMessage(
+      "الرجاء إدخال العنوان",
+    ),
+    "embtyEmailWarning": MessageLookupByLibrary.simpleMessage(
+      "الرجاء إدخال البريد الإلكتروني",
+    ),
+    "embtyFirstnameWarning": MessageLookupByLibrary.simpleMessage(
+      "الرجاء إدخال الاسم الأول",
+    ),
+    "embtyLastnameWarning": MessageLookupByLibrary.simpleMessage(
+      "الرجاء إدخال الاسم الأخير",
+    ),
+    "embtyPasswordWarning": MessageLookupByLibrary.simpleMessage(
+      "الرجاء إدخال كلمة المرور",
+    ),
+    "embtyPhoneWarning": MessageLookupByLibrary.simpleMessage(
+      "الرجاء إدخال رقم الهاتف",
+    ),
+    "feedback": MessageLookupByLibrary.simpleMessage("التقييمات"),
+    "female": MessageLookupByLibrary.simpleMessage("أنثى"),
+    "finish": MessageLookupByLibrary.simpleMessage("إنهاء"),
+    "firstName": MessageLookupByLibrary.simpleMessage("الاسم الأول"),
+    "forgetPasswordInfo": MessageLookupByLibrary.simpleMessage(
+      "أدخل بريدك الإلكتروني لإعادة تعيين كلمة المرور.",
+    ),
+    "gender": MessageLookupByLibrary.simpleMessage("الجنس"),
+    "general": MessageLookupByLibrary.simpleMessage("عام"),
+    "haveAccount": MessageLookupByLibrary.simpleMessage(
+      "هل لديك حساب بالفعل؟ ",
+    ),
+    "hello": MessageLookupByLibrary.simpleMessage("مرحبًا دكتور،"),
+    "helpAndSupport": MessageLookupByLibrary.simpleMessage("المساعدة والدعم"),
+    "history": MessageLookupByLibrary.simpleMessage("السجل"),
+    "home": MessageLookupByLibrary.simpleMessage("الرئيسية"),
+    "homeContainerDescription": MessageLookupByLibrary.simpleMessage(
+      "اقبل طلبات الزيارات المنزلية والرعاية التمريضية من المرضى القريبين.",
+    ),
+    "inValidMailWarning": MessageLookupByLibrary.simpleMessage(
+      "الرجاء إدخال بريد إلكتروني صالح",
+    ),
+    "individual": MessageLookupByLibrary.simpleMessage("فردي"),
+    "insurance": MessageLookupByLibrary.simpleMessage("التأمين"),
+    "joinUs": MessageLookupByLibrary.simpleMessage("انضم إلينا الآن!"),
+    "language": MessageLookupByLibrary.simpleMessage("اللغة"),
+    "languageChangedFailed": MessageLookupByLibrary.simpleMessage(
+      "فشل تغيير اللغة، يرجى المحاولة مرة أخرى",
+    ),
+    "languageChangedSuccessfully": MessageLookupByLibrary.simpleMessage(
+      "تم تغيير اللغة بنجاح",
+    ),
+    "lastName": MessageLookupByLibrary.simpleMessage("الاسم الأخير"),
+    "location": MessageLookupByLibrary.simpleMessage("الموقع"),
+    "login": MessageLookupByLibrary.simpleMessage("تسجيل الدخول"),
+    "loginTitle": MessageLookupByLibrary.simpleMessage("مرحبًا بعودتك"),
+    "logout": MessageLookupByLibrary.simpleMessage("تسجيل الخروج"),
+    "male": MessageLookupByLibrary.simpleMessage("ذكر"),
+    "manageCards": MessageLookupByLibrary.simpleMessage("إدارة البطاقات"),
+    "nationalId": MessageLookupByLibrary.simpleMessage("الرقم القومي"),
+    "nationalIdEmbtyWarning": MessageLookupByLibrary.simpleMessage(
+      "الرجاء إدخال الرقم القومي",
+    ),
+    "nationalIdShortWarning": MessageLookupByLibrary.simpleMessage(
+      "يجب أن يتكون الرقم القومي من 14 رقمًا على الأقل",
+    ),
+    "newPassword": MessageLookupByLibrary.simpleMessage("كلمة المرور الجديدة"),
+    "newPasswordInfo": MessageLookupByLibrary.simpleMessage(
+      "إنشاء كلمة مرور جديدة",
+    ),
+    "noDescriptionText": MessageLookupByLibrary.simpleMessage(
+      "لا يوجد وصف مقدم",
+    ),
+    "noOnngoingRequests": MessageLookupByLibrary.simpleMessage(
+      "لا توجد طلبات جارية",
+    ),
+    "noResponseText": MessageLookupByLibrary.simpleMessage("لا يوجد رد بعد"),
+    "noServices": MessageLookupByLibrary.simpleMessage("لا توجد خدمات متاحة"),
+    "noSubjectText": MessageLookupByLibrary.simpleMessage("لا يوجد موضوع"),
+    "oldPassword": MessageLookupByLibrary.simpleMessage("كلمة المرور القديمة"),
+    "onBoarding1Title": MessageLookupByLibrary.simpleMessage(
+      "انضم إلى شبكة مقدمي الرعاية الصحية لدينا",
+    ),
+    "onBoarding1description": MessageLookupByLibrary.simpleMessage(
+      "قدم خدماتك الطبية في المنزل وتواصل مع المرضى بسهولة.",
+    ),
+    "onBoarding2Title": MessageLookupByLibrary.simpleMessage(
+      "زيارات منزلية سهلة وخدمات طبية",
+    ),
+    "onBoarding2description": MessageLookupByLibrary.simpleMessage(
+      "إدارة المواعيد، زيارة المرضى، والحصول على الدفع - كل ذلك في تطبيق واحد.",
+    ),
+    "onBoarding3Title": MessageLookupByLibrary.simpleMessage(
+      "ابدأ في قبول طلبات الزيارات المنزلية اليوم!",
+    ),
+    "onBoarding3description": MessageLookupByLibrary.simpleMessage(
+      "سجل، تحقق من تفاصيلك، وابدأ في قبول طلبات الزيارات المنزلية.",
+    ),
+    "ongoingRequests": MessageLookupByLibrary.simpleMessage("الطلبات الجارية"),
+    "orLoginWith": MessageLookupByLibrary.simpleMessage(
+      "أو سجل الدخول باستخدام",
+    ),
+    "organizational": MessageLookupByLibrary.simpleMessage("تنظيمي"),
+    "otpInfo": MessageLookupByLibrary.simpleMessage(
+      "تم إرسال رابط إعادة التعيين، يرجى إدخال أرقام OTP هنا للمتابعة.",
+    ),
+    "passwordChangedSuccessfully": MessageLookupByLibrary.simpleMessage(
+      "تم تغيير كلمة المرور بنجاح",
+    ),
+    "patient": MessageLookupByLibrary.simpleMessage("المريض"),
+    "phone": MessageLookupByLibrary.simpleMessage("رقم الهاتف"),
+    "profile": MessageLookupByLibrary.simpleMessage("الملف الشخصي"),
+    "providerGain": MessageLookupByLibrary.simpleMessage("أرباح مقدم الخدمة"),
+    "providerStats": MessageLookupByLibrary.simpleMessage(
+      "إحصائيات مقدم الخدمة",
+    ),
+    "rateUs": MessageLookupByLibrary.simpleMessage("قيمنا"),
+    "readyToReceiveRequests": MessageLookupByLibrary.simpleMessage(
+      "جاهز لاستقبال الطلبات",
+    ),
+    "rejected": MessageLookupByLibrary.simpleMessage("مرفوض"),
+    "requestDetails": MessageLookupByLibrary.simpleMessage("تفاصيل الطلب"),
+    "requestServices": MessageLookupByLibrary.simpleMessage("خدمات الطلب"),
+    "requests": MessageLookupByLibrary.simpleMessage("الطلبات"),
+    "resendCode": MessageLookupByLibrary.simpleMessage("إعادة إرسال الرمز"),
+    "responseTitle": MessageLookupByLibrary.simpleMessage("الرد"),
+    "sehetnaGain": MessageLookupByLibrary.simpleMessage("أرباح صحتنا"),
+    "selectUserType": MessageLookupByLibrary.simpleMessage("اختر نوع المستخدم"),
+    "sendCode": MessageLookupByLibrary.simpleMessage(
+      "إرسال رابط إعادة التعيين",
+    ),
+    "serviceID": MessageLookupByLibrary.simpleMessage("معرف الخدمة:"),
+    "serviceType": MessageLookupByLibrary.simpleMessage("نوع الخدمة"),
+    "services": MessageLookupByLibrary.simpleMessage("الخدمات"),
+    "shortAddressWarning": MessageLookupByLibrary.simpleMessage(
+      "يجب أن يتكون العنوان من 12 حرفًا على الأقل",
+    ),
+    "shortFirstnameWarning": MessageLookupByLibrary.simpleMessage(
+      "يجب أن يتكون الاسم الأول من 3 أحرف على الأقل",
+    ),
+    "shortLastnameWarning": MessageLookupByLibrary.simpleMessage(
+      "يجب أن يتكون الاسم الأخير من 3 أحرف على الأقل",
+    ),
+    "shortPasswordWarning": MessageLookupByLibrary.simpleMessage(
+      "يجب أن تتكون كلمة المرور من 6 أحرف على الأقل",
+    ),
+    "shortPhoneWarning": MessageLookupByLibrary.simpleMessage(
+      "يجب أن يتكون رقم الهاتف من 11 رقمًا على الأقل",
+    ),
+    "showDetails": MessageLookupByLibrary.simpleMessage("عرض التفاصيل"),
+    "signUp": MessageLookupByLibrary.simpleMessage("اشتراك"),
+    "signUpWith": MessageLookupByLibrary.simpleMessage("أو اشترك باستخدام"),
     "skip": MessageLookupByLibrary.simpleMessage("تخطي"),
+    "statusClosed": MessageLookupByLibrary.simpleMessage("مغلق"),
+    "statusInProgress": MessageLookupByLibrary.simpleMessage("قيد التقدم"),
+    "statusOpen": MessageLookupByLibrary.simpleMessage("مفتوح"),
+    "statusPending": MessageLookupByLibrary.simpleMessage("قيد الانتظار"),
+    "statusResolved": MessageLookupByLibrary.simpleMessage("تم الحل"),
+    "startAccept": MessageLookupByLibrary.simpleMessage(
+      "ابدأ في قبول طلباتك اليوم!",
+    ),
+    "subjectFieldHint": MessageLookupByLibrary.simpleMessage(
+      "أدخل موضوع الشكوى",
+    ),
+    "subjectFieldLabel": MessageLookupByLibrary.simpleMessage("الموضوع"),
+    "subjectValidationError": MessageLookupByLibrary.simpleMessage(
+      "الرجاء إدخال موضوع",
+    ),
+    "submitButtonLabel": MessageLookupByLibrary.simpleMessage("إرسال الشكوى"),
+    "submitYourComplaint": MessageLookupByLibrary.simpleMessage("قدم شكواك"),
+    "todayGain": MessageLookupByLibrary.simpleMessage("إيرادات اليوم"),
+    "totalFees": MessageLookupByLibrary.simpleMessage("إجمالي الرسوم"),
+    "underReview": MessageLookupByLibrary.simpleMessage("قيد المراجعة"),
+    "unknownService": MessageLookupByLibrary.simpleMessage("خدمة غير معروفة"),
+    "uploadDocuments": MessageLookupByLibrary.simpleMessage("تحميل المستندات"),
+    "uploadHere": MessageLookupByLibrary.simpleMessage("تحميل هنا"),
+    "uploadRequiredDocuments": MessageLookupByLibrary.simpleMessage(
+      "تحميل المستندات المطلوبة",
+    ),
+    "userType": MessageLookupByLibrary.simpleMessage("نوع المستخدم"),
+    "userTypeWarning": MessageLookupByLibrary.simpleMessage(
+      "الرجاء تحديد نوع المستخدم",
+    ),
+    "viewDetails": MessageLookupByLibrary.simpleMessage("عرض التفاصيل"),
+    "weWillReviewYourComplaintPrompt": MessageLookupByLibrary.simpleMessage(
+      "سنراجع شكواك وسنعاود الاتصال بك قريبًا",
+    ),
+    "welcomeBack": MessageLookupByLibrary.simpleMessage("مرحبًا بعودتك"),
+    "welcomeToSehetnaApp": MessageLookupByLibrary.simpleMessage(
+      "مرحبًا بك في تطبيق صحتنا",
+    ),
   };
 }
